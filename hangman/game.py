b7510a0506df125c97b5a933f2e59413c39730d3
Charles Cognato <charlescognato@gmail.com>
2021-03-17 17:06:04 -0400

    Refactored to use more signals

--- hangman/game.py
+++ hangman/game.py
@@ -3,12 +3,14 @@ from enum import IntEnum
 from pathlib import Path
 
 from PyQt5.QtCore import QObject, pyqtSignal
+from PyQt5.QtGui import QPixmap
 
-from hangman.wordlist import WordList
+from hangman.config import config
+from hangman.wordlist import LengthFilter, WordList
 
 
-class GameState(IntEnum):
-    GALLOWS = 0
+class GallowsImage(IntEnum):
+    THE_DREADED_GALLOWS = 0
     HEAD = 1
     TORSO = 2
     LEFT_ARM = 3
@@ -17,15 +19,29 @@ class GameState(IntEnum):
     LEFT_LEG = 6
 
 
-def _advance_game_state():
-    for state in GameState:
-        yield state
+_body = config.body
+_image_paths = {
+    GallowsImage.THE_DREADED_GALLOWS: f"resources/images/{_body}/gallows.png",
+    GallowsImage.HEAD: f"resources/images/{_body}/head.png",
+    GallowsImage.TORSO: f"resources/images/{_body}/torso.png",
+    GallowsImage.RIGHT_ARM: f"resources/images/{_body}/leftarm.png",
+    GallowsImage.LEFT_ARM: f"resources/images/{_body}/rightarm.png",
+    GallowsImage.RIGHT_LEG: f"resources/images/{_body}/rightleg.png",
+    GallowsImage.LEFT_LEG: f"resources/images/{_body}/leftleg.png"
+}
+
+
+def _advance_gallows_image():
+    for image in GallowsImage:
+        yield image
 
 
 class Game(QObject):
+    availableLetters = pyqtSignal(str)
     guessedLettersUpdated = pyqtSignal(str)
-    updateProgress = pyqtSignal(tuple)
-    gameOver = pyqtSignal(tuple)
+    imageChanged = pyqtSignal(QPixmap)
+    maskChanged = pyqtSignal(str)
+    gameOver = pyqtSignal(str)
     gameWon = pyqtSignal(str)
 
     extra = {"classname": "Game"}
@@ -38,13 +54,20 @@ class Game(QObject):
         self._logger = logging.getLogger("hangman")
 
         self._word_list = WordList.create_from_file(Path("hangman/words.txt"))
+        if config.length and config.length > 0:
+            word_length = LengthFilter(config.length)
+            self._word_list.add_filter(word_length)
         self._guessed_letters = []
         self._available_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         self._word_to_guess = None
         self._game_over = True
         self._game_state = None
-        self._current_state = None
+        self._current_image = None
         self._mask = []
+
+    @property
+    def mask(self) -> str:
+        return self._join(self._mask, "")
 
     @property
     def word(self) -> str:
@@ -57,12 +80,17 @@ class Game(QObject):
         self._available_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         self._guessed_letters = []
         self._game_over = False
-        self._game_state = _advance_game_state()
-        self._current_state = next(self._game_state)
-        self._emit_progress_update()
+        self._game_state = _advance_gallows_image()
+        self._current_image = next(self._game_state)
+
+        # noinspection PyUnresolvedReferences
+        self.maskChanged.emit(self.mask)
+        # noinspection PyUnresolvedReferences
+        self._emit_image_changed(GallowsImage.THE_DREADED_GALLOWS)
 
     def process_guess(self, letter) -> None:
         self._logger.info(f"processing guess: '{letter}'", extra=self.extra)
+
         if self._game_over or letter in self._guessed_letters:
             return
 
@@ -70,53 +98,42 @@ class Game(QObject):
         self._remove_letter_choice(letter)
 
         if self._word_contains_letter(letter):
-            self._reveal_guess(letter)
-            self._emit_progress_update()
+            self._update_mask(letter)
 
             if self._did_win():
                 self._game_over = True
                 # noinspection PyUnresolvedReferences
-                self.gameOver.emit(("WON", self._word_to_guess))
+                self.gameOver.emit("WON")
         else:
-            self._do_wrong_guess()
+            self._process_wrong_guess()
 
     # **********  PRIVATE  INTERFACE  **********************************************************************
 
     def _did_win(self) -> bool:
-        return self._get_mask() == self._word_to_guess
+        return self.mask == self._word_to_guess
 
     def _is_game_lost(self):
-        self._current_state: GameState = next(self._game_state)
-        return self._current_state.value == 6
+        self._current_image: GallowsImage = next(self._game_state)
+        return self._current_image == GallowsImage.LEFT_LEG
 
-    def _do_wrong_guess(self):
+    def _process_wrong_guess(self):
         if self._is_game_lost():
-            self._emit_progress_update()
             # noinspection PyUnresolvedReferences
-            self.gameOver.emit(("LOST", self._word_to_guess))
+            self.gameOver.emit("LOST")
             self._game_over = True
-        else:
-            self._emit_progress_update()
 
-    def _emit_progress_update(self):
         # noinspection PyUnresolvedReferences
-        self.updateProgress.emit(
-            (
-                self._current_state,
-                self._get_available_letters(),
-                self._get_guessed_letters(),
-                self._get_mask()
-            )
-        )
+        self._emit_image_changed(self._current_image)
+
+    def _emit_image_changed(self, index: GallowsImage):
+        # noinspection PyUnresolvedReferences
+        self.imageChanged.emit(QPixmap(_image_paths[index]))
 
     def _get_available_letters(self):
         return self._join(self._available_letters, " ")
 
     def _get_guessed_letters(self):
         return self._join(self._guessed_letters, " ")
-
-    def _get_mask(self):
-        return self._join(self._mask, "")
 
     def _record_letter(self, letter: str) -> None:
         self._guessed_letters.append(letter)
@@ -125,8 +142,10 @@ class Game(QObject):
 
     def _remove_letter_choice(self, letter) -> None:
         self._available_letters = self._available_letters.replace(letter, "")
+        # noinspection PyUnresolvedReferences
+        self.availableLetters.emit(self._get_available_letters())
 
-    def _reveal_guess(self, letter: str) -> None:
+    def _update_mask(self, letter: str) -> None:
         indexes = []
         for index, char in enumerate(self._word_to_guess):
             if letter == char:
@@ -134,6 +153,9 @@ class Game(QObject):
 
         for index in indexes:
             self._mask[index] = letter
+
+        # noinspection PyUnresolvedReferences
+        self.maskChanged.emit(self.mask)
 
     def _word_contains_letter(self, letter: str) -> bool:
         return letter in self._word_to_guess
--- hangman/mainwindow.py
+++ hangman/mainwindow.py
@@ -1,27 +1,18 @@
 import logging
 
 from PyQt5 import QtGui
-from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtCore import QTimer, Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QDialog
 from icecream import ic
 
 from . import helpers
+from .config import config
 from .designerforms import Ui_MainDialog
-from .game import Game, GameState
+from .game import Game
 from .resultdialog import ResultDialog
 
 ic.configureOutput(includeContext=True)
-
-_image_paths = {
-    GameState.GALLOWS: "resources/images/gallows-0.png",
-    GameState.HEAD: "resources/images/gallows-1.png",
-    GameState.TORSO: "resources/images/gallows-2.png",
-    GameState.LEFT_ARM: "resources/images/gallows-3.png",
-    GameState.RIGHT_ARM: "resources/images/gallows-4.png",
-    GameState.RIGHT_LEG: "resources/images/gallows-5.png",
-    GameState.LEFT_LEG: "resources/images/gallows-6.png"
-}
 
 _image_logo_path = "resources/images/Logo_1.png"
 _image_win_path = "resources/images/win.png"
@@ -33,25 +24,24 @@ _alphabet = frozenset("abcdefghijklmnopqrstuvwxyz")
 class MainWindow(QDialog, Ui_MainDialog):
     extra = {"classname": "MainWindow"}
 
-    def __init__(self, args: dict):
+    def __init__(self):
         super().__init__()
         self.resize(1000, 785)
         self.setupUi(self)
 
         self._logger = logging.getLogger("hangman")
 
-        self._args = args
-
         self._ctrl_f = helpers.CtrlKeySequence(Qt.Key_F)
         self._ctrl_n = helpers.CtrlKeySequence(Qt.Key_N)
         self._ctrl_r = helpers.CtrlKeySequence(Qt.Key_R)
 
         self._game = Game()
-        self._game.guessedLettersUpdated.connect(
-            lambda guessed_letters: self.label_guessed_letters.setText(guessed_letters)
-        )
-        self._game.updateProgress.connect(self._update_view)
+        self._game.availableLetters.connect(self.label_available_letters.setText)
+        self._game.guessedLettersUpdated.connect(self.label_guessed_letters.setText)
+        self._game.imageChanged.connect(self.label_status.setPixmap)
+        self._game.maskChanged.connect(self.label_word.setText)
         self._game.gameOver.connect(self._game_over)
+        self._game.gameOver.connect(lambda result: self.label_word.setText(self._game.word))
 
         self._result_dialog = ResultDialog(self)
 
@@ -61,7 +51,7 @@ class MainWindow(QDialog, Ui_MainDialog):
         self.pb_show_world.clicked.connect(lambda: print(f"word = {self._game.word}"))
 
     def closeEvent(self, event: QtGui.QCloseEvent) -> None:
-        if self._args["noconfirmexit"] or helpers.pop_exit_dialog(self):
+        if config.noconfirmexit or helpers.pop_exit_dialog(self):
             event.accept()
         else:
             event.ignore()
@@ -85,19 +75,9 @@ class MainWindow(QDialog, Ui_MainDialog):
     def _new_game(self) -> None:
         self._game.new_game()
 
-    def _game_over(self, status: tuple[str, str]) -> None:
-        result, the_word = status
+    def _game_over(self, result: str) -> None:
         image_path = _image_win_path if result == "WON" else _image_lose_path
-        QTimer.singleShot(500, lambda: self._show_result_dialog(QPixmap(image_path)))
-        self.label_word.setText(the_word)
+        QTimer.singleShot(0, lambda: self._show_result_dialog(QPixmap(image_path)))
 
     def _show_result_dialog(self, image: QPixmap) -> None:
         self._result_dialog.run(image)
-
-    def _update_view(self, state: tuple[GameState, str, str, str]) -> None:
-        image, available_letters, guessed_letters, mask = state
-        self.label_status.setPixmap(QPixmap(_image_paths[image]))
-        self._logger.debug(f"setting image to {image.name}", extra=self.extra)
-        self.label_available_letters.setText(available_letters)
-        self.label_guessed_letters.setText(guessed_letters)
-        self.label_word.setText(mask)
