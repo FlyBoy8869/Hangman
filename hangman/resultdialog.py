72b6e30511a21f9c8af8b6c468b24495ed538664
FlyBoy8869 <charlescognato@gmail.com>
2021-03-23 07:12:28 -0400

    Specified a more specific window type

--- hangman/resultdialog.py
+++ hangman/resultdialog.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtGui
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QDialog
 
@@ -9,11 +10,11 @@ class ResultDialog(QDialog, Ui_ResultDialog):
     def __init__(self, parent):
         super().__init__(parent)
         self.setupUi(self)
-        self.setWindowTitle("Game Over")
+        self.setWindowFlag(Qt.FramelessWindowHint)
 
-    def run(self, image: QPixmap) -> int:
+    def run(self, image: QPixmap) -> None:
         self.label_result.setPixmap(image)
-        return super().exec()
+        super().exec()
 
     def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
         self.accept()
