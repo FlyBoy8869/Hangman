14bb8fe5139e01ca16ba1891409d8a96f0cbe37d
FlyBoy8869 <charlescognato@gmail.com>
2021-03-30 07:50:33 -0400

    Passing a file object now so I can use StringIO in tests

--- hangman/wordpicker.py
+++ hangman/wordpicker.py
@@ -9,29 +9,39 @@ from hangman.filters import FilterCollection
 class _WordPicker(QObject):
     publish_word = pyqtSignal(str)
 
-    def __init__(self, word_count, path, filters=None):
+    # def __init__(self, word_count, path, filters=None):
+    def __init__(self, word_count, file_obj: IO[str], filters=None):
         super().__init__()
         self._word_count = word_count
-        self._path = path
+        # self._path = path
+        self._file_obj = file_obj
         self._filters = filters
 
     def pick(self):
         while True:
+            # word = self._get_word(
+            #     at_index=self._get_index(self._word_count),
+            #     from_path=self._path
+            # )
             word = self._get_word(
                 at_index=self._get_index(self._word_count),
-                from_path=self._path
+                from_file=self._file_obj
             )
             if word and not self._filters:
                 break
             if self._filters.apply(word):
                 break
 
+            self._file_obj.seek(0)
+
         # noinspection PyUnresolvedReferences
         self.publish_word.emit(word.upper())
+        self._file_obj.close()
 
-    def _get_word(self, *, at_index: int, from_path: str) -> str:
-        with open(from_path, "r") as infile:
-            word = self._find_word_in_file(at_index, infile)
+    # def _get_word(self, *, at_index: int, from_path: str) -> str:
+    def _get_word(self, *, at_index: int, from_file: IO[str]) -> str:
+        # with open(from_path, "r") as infile:
+        word = self._find_word_in_file(at_index, from_file)
         return word
 
     @staticmethod
@@ -67,7 +77,9 @@ class WordPicker(QObject):
         self._filters = filters
 
     def pick_a_word(self):
-        picker = _WordPicker(self._word_count, self._path, self._filters)
+        file = open(self._path, "r")
+        # picker = _WordPicker(self._word_count, self._path, self._filters)
+        picker = _WordPicker(self._word_count, file, self._filters)
         # noinspection PyUnresolvedReferences
         picker.publish_word.connect(self._receive_word)
         word_picker = _WordPickerRunnable(picker)
